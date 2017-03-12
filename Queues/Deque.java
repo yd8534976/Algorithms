8023fdf662afdbcc322ec4dc43d0efd7a0d1be4b
CaTheother <yd8534976@gmail.com>
2017-03-12 16:39:12 +0900

    CheckStyle

--- Queues/Deque.java
+++ Queues/Deque.java
@@ -5,109 +5,123 @@ import java.util.NoSuchElementException;
  * Created by CaTheother on 3/2/17.
  */
 public class Deque<Item> implements Iterable<Item> {
-    private class Node{
-        protected Node(Item it){
-            item=it;
+    private class Node {
+        protected Node(Item it) {
+            item = it;
         }
-        protected Item item=null;
-        protected Node next=null;
-        protected Node previous=null;
+
+        protected Item item = null;
+        protected Node next = null;
+        protected Node previous = null;
     }
-    private Node first=null;
-    private Node last=null;
-    private int N=0;
+
+    private Node first = null;
+    private Node last = null;
+    private int N = 0;
     private Item i;
-    public Deque(){
+
+    public Deque() {
     }
-    public boolean isEmpty(){
-        if (N==0){
+
+    public boolean isEmpty() {
+        if (N == 0) {
             return true;
         }
         return false;
     }
-    public int size(){
+
+    public int size() {
         return N;
     }
-    public void addFirst(Item item){
-        if (item==null){
+
+    public void addFirst(Item item) {
+        if (item == null) {
             throw new NullPointerException();
         }
-        Node node=new Node(item);
-        node.next=first;
-        if (first!=null){
-            first.previous=node;
+        Node node = new Node(item);
+        node.next = first;
+        if (first != null) {
+            first.previous = node;
         }
-        first=node;
-        if (N==0){
-            last=node;
+        first = node;
+        if (N == 0) {
+            last = node;
         }
         N++;
     }
-    public void addLast(Item item){
-        if (item==null){
+
+    public void addLast(Item item) {
+        if (item == null) {
             throw new NullPointerException();
         }
-        Node node=new Node(item);
-        node.previous=last;
-        if (last!=null){
-            last.next=node;
+        Node node = new Node(item);
+        node.previous = last;
+        if (last != null) {
+            last.next = node;
         }
-        last=node;
-        if (N==0){
-            first=node;
+        last = node;
+        if (N == 0) {
+            first = node;
         }
         N++;
 
     }
-    public Item removeFirst(){
-        if (first==null){
+
+    public Item removeFirst() {
+        if (first == null) {
             throw new NoSuchElementException();
         }
-        Item item=first.item;
-        first=first.next;
+        Item item = first.item;
+        first = first.next;
         N--;
-        if (N==0){
-            last=null;
+        if (N == 0) {
+            last = null;
             return item;
         }
-        first.previous=null;
+        first.previous = null;
         return item;
     }
-    public Item removeLast(){
-        if (last==null){
+
+    public Item removeLast() {
+        if (last == null) {
             throw new NoSuchElementException();
         }
-        Item item=last.item;
-        last=last.previous;
+        Item item = last.item;
+        last = last.previous;
         N--;
-        if (N==0){
-            first=null;
+        if (N == 0) {
+            first = null;
             return item;
         }
-        last.next=null;
+        last.next = null;
         return item;
     }
-    public Iterator<Item> iterator(){
+
+    public Iterator<Item> iterator() {
         return new dequeIterator();
     }
-    private class dequeIterator implements Iterator<Item>{
-        private Node current=first;
-        public boolean hasNext(){
-            return current!=null;
+
+    private class dequeIterator implements Iterator<Item> {
+        private Node current = first;
+
+        public boolean hasNext() {
+            return current != null;
         }
-        public Item next(){
-            if (current==null){
+
+        public Item next() {
+            if (current == null) {
                 throw new NoSuchElementException();
             }
-            Item item=current.item;
-            current=current.next;
+            Item item = current.item;
+            current = current.next;
             return item;
         }
-        public void remove(){
+
+        public void remove() {
             throw new UnsupportedOperationException();
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
     }
 }
--- Queues/RandomizedQueue.java
+++ Queues/RandomizedQueue.java
@@ -8,118 +8,129 @@ import edu.princeton.cs.algs4.StdRandom;
  */
 public class RandomizedQueue<Item> implements Iterable<Item> {
     private Item[] a;
-    private int N=0;//the number of items
-    private int size=2;//the size of array
-    private int first=0;//first position of queue
-    private int last=-1;//last position of queue
+    private int N = 0;//the number of items
+    private int size = 2;//the size of array
+    private int first = 0;//first position of queue
+    private int last = -1;//last position of queue
     //if initial last=0:
     // after first call of enqueue(),Error:first=0,last=1,N=1;
 
-    private void reSize(int newsize){
+    private void reSize(int newsize) {
         //if (N==size): newsize=size*2;
         //if (N==size/4)and(N>0): newsize=size/2;
-            Item[] newa = (Item[]) new Object[newsize];
-            for (int i = 0; i < N; i++) {
-                newa[i] = a[(first + i) % size];
-            }
-            a = newa;
-            size =newsize;
-            first = 0;
-            last = N - 1;
-            //reset:
-            //newa[0] will be the first of queue;
+        Item[] newa = (Item[]) new Object[newsize];
+        for (int i = 0; i < N; i++) {
+            newa[i] = a[(first + i) % size];
+        }
+        a = newa;
+        size = newsize;
+        first = 0;
+        last = N - 1;
+        //reset:
+        //newa[0] will be the first of queue;
     }
 
-    private int getRandomPosition(){
+    private int getRandomPosition() {
         //get a random position from the valid area
-        int x=StdRandom.uniform(0,N);
-        x=(first+x)%size;
+        int x = StdRandom.uniform(0, N);
+        x = (first + x) % size;
         return x;
     }
 
-    public RandomizedQueue(){
-        a=(Item[]) new Object[size];
+    public RandomizedQueue() {
+        a = (Item[]) new Object[size];
     }
-    public boolean isEmpty(){
-        if (N==0){
+
+    public boolean isEmpty() {
+        if (N == 0) {
             return true;
         }
         return false;
     }
-    public int size(){
+
+    public int size() {
         return N;
     }
-    public void enqueue(Item item){
-        if (item==null){
+
+    public void enqueue(Item item) {
+        if (item == null) {
             throw new NullPointerException();
         }
         last++;
-        last=last%size;//if last==size: a[0] will be the last
-        a[last]=item;
+        last = last % size;//if last==size: a[0] will be the last
+        a[last] = item;
         N++;
-        if (N==size){//if full, double size
-            reSize(size*2);
+        if (N == size) {//if full, double size
+            reSize(size * 2);
         }
     }
-    public Item dequeue(){
-        if (N==0){
+
+    public Item dequeue() {
+        if (N == 0) {
             throw new NoSuchElementException();
         }
-        int x=getRandomPosition();
-        Item item=a[x];
-        a[x]=a[first];
-        a[first]=null;
+        int x = getRandomPosition();
+        Item item = a[x];
+        a[x] = a[first];
+        a[first] = null;
         first++;
         N--;
-        first=first%size;
-        if ((N==size/4)&&(N>0)){//release loitering memories
-            reSize(size/2);
+        first = first % size;
+        if ((N == size / 4) && (N > 0)) {//release loitering memories
+            reSize(size / 2);
         }
         return item;
     }
-    public Item sample(){
-        if (N==0){
+
+    public Item sample() {
+        if (N == 0) {
             throw new NoSuchElementException();
         }
-        int x=getRandomPosition();
+        int x = getRandomPosition();
         return a[x];
 
     }
-    public Iterator<Item> iterator(){
+
+    public Iterator<Item> iterator() {
         return new QueueIterator();
 
     }
-    private class QueueIterator implements Iterator<Item>{
-        private int current=0;
+
+    private class QueueIterator implements Iterator<Item> {
+        private int current = 0;
         private Item[] b;
-        public QueueIterator(){
-            b=(Item[]) new Object[N];
-            for (int i=0;i<N;i++){
-                b[i]=a[(first+i)%size];
+
+        public QueueIterator() {
+            b = (Item[]) new Object[N];
+            for (int i = 0; i < N; i++) {
+                b[i] = a[(first + i) % size];
             }
         }
-        public void remove(){
+
+        public void remove() {
             throw new UnsupportedOperationException();
         }
 
-        public boolean hasNext(){
-            if (current==N){
+        public boolean hasNext() {
+            if (current == N) {
                 return false;
             }
             return true;
         }
-        public Item next(){
-            if (current==N){
+
+        public Item next() {
+            if (current == N) {
                 throw new NoSuchElementException();
             }
-            int x=StdRandom.uniform(current,N);//[0,current) is the invalid area
-            Item item=b[x];
-            b[x]=b[current];//after using b[x],b[x] will be replaced by b[current]
+            int x = StdRandom.uniform(current, N);//[0,current) is the invalid area
+            Item item = b[x];
+            b[x] = b[current];//after using b[x],b[x] will be replaced by b[current]
             current++;//so old b[current] will still be in the valid area
             return item;
         }
 
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
     }
 }
